186c9f30d3043820b687d6a96d5e70f4376c157f
Nic Townsend <nictownsend@uk.ibm.com>
2022-07-11 13:09:20 +0100

    updating latest

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for f in dockerfiles/*
+BASE_DIR=$(dirname "$(readlink -f "$0")")
+
+for f in "${BASE_DIR}"/dockerfiles/*
 do
   FILENAME=$(basename "${f}" | tr "[:upper:]" "[:lower:]")
   docker build -f "$f" -t "localhost:5000/${FILENAME%%.*}" .
